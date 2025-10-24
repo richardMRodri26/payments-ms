c113a09dbdfa4ee9de75b864bc76ec01cd3e7ae0
RichardRodri26 <richardmanuel811@gmail.com>
2025-10-23 22:14:41 -0500

    feat: add NATS microservices support and update environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,7 @@ export interface EnvVars {
   STRIPE_ENDPOINT_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object<EnvVars>({
@@ -15,11 +16,13 @@ const envsSchema = joi.object<EnvVars>({
   STRIPE_ENDPOINT_SECRET: joi.string().required(),
   STRIPE_SUCCESS_URL: joi.string().uri().required(),
   STRIPE_CANCEL_URL: joi.string().uri().required(),
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
 })
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
   ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
@@ -36,4 +39,5 @@ export const envs = {
   stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  natsServers: envVars.NATS_SERVERS,
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms')
@@ -15,8 +16,16 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
     })
-  )
+  );
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    }
+  })
+
+  await app.startAllMicroservices();
   await app.listen(envs.port);
 
   logger.log(`Application is running on: ${envs.port}`);
